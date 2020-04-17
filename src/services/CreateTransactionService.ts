ce974eedeb93b45e9936f7a6910e5fbbe31f19fd
Ronnie Princi <rprinci@gmail.com>
2020-04-17 17:54:48 +0200

    add transaction type validation

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -15,10 +15,14 @@ class CreateTransactionService {
   }
 
   public execute({ title, value, type }: Request): Transaction {
+    if (!['income', 'outcome'].includes(type)) {
+      throw new Error('Transaction type is invalid');
+    }
+
     const { total } = this.transactionsRepository.getBalance();
 
     if (type === 'outcome' && total < value) {
-      throw Error('Your balance is too low to make this transaction');
+      throw new Error('Your balance is too low to make this transaction');
     }
 
     const transaction = this.transactionsRepository.create({
